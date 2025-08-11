c3a232b059d31491378b5ace8855d5d5b11fa4dd
Nikita Gritsunov <gritsunovnv@yandex.ru>
2025-08-11 07:38:55 +0300

    FlashChat app complet

---
+++ Module_15/Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Никита Грицунов on 09.08.2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
